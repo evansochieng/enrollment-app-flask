52981854705c997590c5a265945d6972f3722347
evans ochieng <evansochieng121@gmail.com>
2023-06-25 02:08:30 +0300

    create enrollment app

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-# import the flask class
-from flask import Flask
-
-# create an app
-# __name__ is the special name for the current application/variable being rendered
-app = Flask(__name__)
-
-# create simple routes
-# @ is a decorator
-@app.route("/")
-@app.route("/home")
-
-def index():
-    return "<h1>Hello, Evans! This is a first great step!</h1>"
+# import the app from __init__.py file
+from application import app
