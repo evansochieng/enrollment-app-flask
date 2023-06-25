7d653878bd8bba3de9e8b7f1b987b0dc9f22218e
evans ochieng <evansochieng121@gmail.com>
2023-06-25 09:54:30 +0300

    create home page

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,10 @@
 from application import app
-
+from flask import render_template
 # create simple routes
 # @ is a decorator
 @app.route("/")
 @app.route("/index")
 
 def index():
-    return "<h1>Hello, Evans! This is a first great step!</h1>"
+    # render html template from templates folder
+    return render_template("index.html")
